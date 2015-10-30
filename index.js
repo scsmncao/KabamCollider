5aecabf88e2ff6c36aa300147b4407b89e52f520
Simon Cao <scsmncao@gmail.com>
2015-10-29 22:22:03 -0700

    fixed functions

--- index.js
+++ index.js
@@ -33,8 +33,93 @@ var listOfCountriesAndPPP = {'Australia': 114,
 								'United Kingdom': 122,
 								'United States': 100 };
 
+var listOfCountriesAndBigMac = {'Australia': 4.81,
+								'Austria': 4.56,
+								'Belgium': 4.98,
+								'Canada': 4.54,
+								'Chile': 3.27,
+								'Czech Republic': 2.83,
+								'Denmark': 5.08,
+								'Estonia': 3.34,
+								'Finland': 5.27,
+								'France': 5.25,
+								'Germany': 4.94,
+								'Greece': 3.70,
+								'Hungary': 3.18,
+								'Iceland': 5.29,
+								'Ireland': 4.45,
+								'Israel': 4.63,
+								'Italy': 4.82,
+								'Japan': 2.99,
+								'Korea': 3.76,
+								'Luxembourg': 4.89,
+								'Mexico': 3.11,
+								'Netherlands': 4.90,
+								'New Zealand': 3.91,
+								'Norway': 5.65,
+								'Poland': 2.54,
+								'Portugal': 4.04,
+								'Slovak Republic': 3.34,
+								'Slovenia': 2.56,
+								'Spain': 4.54,
+								'Sweden': 5.15,
+								'Switzerland': 6.82,
+								'Turkey': 3.87,
+								'United Kingdom': 4.51,
+								'United States': 4.79 };
+
+result = false;
+chart = false;
+
+
+google.load('visualization', '1.1', {'packages': ['line']});
 
 $(document).ready(loadPage);
+
+function draw() {
+	google.setOnLoadCallback(drawChart);
+
+      function drawChart () {
+
+        var data = new google.visualization.DataTable();
+        data.addColumn('string', 'Country');
+        data.addColumn('number', 'PPP');
+        data.addColumn('number', 'Big Mac Index');
+
+        var listOfData = []
+
+        $.each(Object.keys(listOfCountriesAndPPP), function(key, value) {
+        	var ratioPPP = (listOfCountriesAndPPP[value] * 1.0) / listOfCountriesAndPPP['United States'];
+        	var ratioBigMac = (listOfCountriesAndBigMac[value] * 1.0) / listOfCountriesAndBigMac['United States'];
+        	listOfData.push([value, ratioPPP, ratioBigMac]);
+        });
+
+        data.addRows(listOfData);
+
+        var options = {
+          chart: {
+            title: 'PPP Compared to Big Mac Index'
+          },
+          width: 1400,
+          height: 500,
+          series: {
+            0: {axis: 'PPP'},
+            1: {axis: 'Big Mac Index'}
+          },
+          axes: {
+            y: {
+              'PPP': {label: 'PPP'},
+              'Big Mac Index': {label: 'Big Mac Index'}
+            }
+          }
+        };
+
+        var chart = new google.charts.Line(document.getElementById('chart'));
+
+        chart.draw(data, options);
+
+      }
+}
 
 function loadPage() {
 	var countries = $('.countries');
@@ -46,20 +131,43 @@ function loadPage() {
 	});
 	$('.calculate').click(calculatePPP);
 	$('.see-all').click(seeAll);
-	$('.see-percentage').click(calculatePercentEntertainment)
+	$('.see-percentage').click(calculatePercentEntertainment);
+	$('.see-chart').click(function() {
+		if (chart) {
+			$('.chart').css('opacity', 0);
+			chart = false;
+		}
+		else {
+			$('.chart').css('opacity', 1);
+			chart = true;
+		}
+	});
+	draw();
 }
 
 function calculatePPP() {
+	if (result) {
+		$('.result').empty();
+		result = false;
+		return;
+	}
+	result = true;
 	var country = $('.countries option:selected').text();
 	var pppValue = listOfCountriesAndPPP[country];
 	var usdAmount = parseFloat($('#USD').val());
 	var finalAmount = Number((usdAmount * (pppValue/100.0)).toFixed(2));
 	$('.result').empty()
-	$('.result').append('An item that costs ' + '<b>$' + usdAmount + '</b> in the <b>United States</b> should cost ' + '<b>$' + finalAmount + '</b> in <b>' + country + '</b>.');
+	$('.result').append('An item that costs ' + '<b>' + usdAmount + ' Units</b> in the <b>United States</b> should cost ' + '<b>' + finalAmount + ' Units</b> in <b>' + country + '</b>.');
 }
 
 function seeAll() {
+	if (result) {
+		$('.result').empty();
+		result = false;
+		return;
+	}
 	$('.result').empty()
+	result = true;
 	var usdAmount = parseFloat($('#USD').val());
 	var sumAmount = 0;
 	$.each(Object.keys(listOfCountriesAndPPP), function(key, value) {
@@ -67,12 +175,18 @@ function seeAll() {
 		var countryPPP = listOfCountriesAndPPP[country];
 		var finalAmount = Number((usdAmount * (countryPPP/100.0)).toFixed(2));
 		sumAmount += finalAmount;
-		$('.result').append('An item that costs ' + '<b>$' + usdAmount + '</b> in the <b>United States</b> should cost ' + '<b>$' + finalAmount + '</b> in <b>' + country + '</b>.<br>');
+		$('.result').append('An item that costs ' + '<b>' + usdAmount + ' Units</b> in the <b>United States</b> should cost ' + '<b>' + finalAmount + ' Units</b> in <b>' + country + '</b>.<br>');
 	});
 	$('.result').append('The sum of all the costs is ' + '<b>$' + sumAmount + '</b> in ' + Object.keys(listOfCountriesAndPPP).length + ' countries.');
 }
 
 function calculatePercentEntertainment() {
+	if (result) {
+		$('.result').empty();
+		result = false;
+		return;
+	}
+	result = true;
 	$('.result').empty()
 	$.getJSON("revenueByCountry.json", function(revenue) {
     	$.getJSON("internetPopulation.json", function(population) {
